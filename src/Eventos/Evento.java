fe2a96950bcce0f99a7e217367f05e3755282a68
katgf <>
2020-08-18 03:53:59 -0600

    agregada clase Evento

---
+++ src/Eventos/Evento.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Eventos;
+
+/**
+ *
+ * @author Admin
+ */
+public abstract class Evento {
+    private String codigo;
+    private double Precio;
+    private String Tipo;
+
+    public Evento() {
+    }
+
+    public double getPrecio() {
+        return Precio;
+    }
+
+    public void setPrecio(double Precio) {
+        this.Precio = Precio;
+    }
+
+    public String getTipo() {
+        return Tipo;
+    }
+
+    public void setTipo(String Tipo) {
+        this.Tipo = Tipo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+    
+    public abstract String TextToFile();
+    public abstract void SetFromFile(String datos);
+}
