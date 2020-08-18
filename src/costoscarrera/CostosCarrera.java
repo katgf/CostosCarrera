c16542a7c709e09479da5bbeadac9852b5505849
katgf <>
2020-08-18 03:47:12 -0600

    Agregados eventos

--- src/costoscarrera/CostosCarrera.java
+++ src/costoscarrera/CostosCarrera.java
@@ -5,6 +5,12 @@
  */
 package costoscarrera;
 
+import Carrera.Carrera;
+import Carrera.Periodo;
+import Eventos.Clase;
+import Eventos.Evento;
+import java.util.ArrayList;
+
 /**
  *
  * @author Admin
@@ -16,6 +22,45 @@ public class CostosCarrera {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Carrera c = new Carrera();
+        c.setNombre("Ing Financiera");
+        c.setAñoInicio(2007);
+        
+        ArrayList<Periodo> periodos = new ArrayList<Periodo>();
+        Periodo p = new Periodo();
+        Periodo p2 = new Periodo();
+        p.setAño(2007);
+        p.setPeriodo(1);
+        periodos.add(p);
+        p2.setAño(2007);
+        p2.setPeriodo(2);
+        
+        Clase cl = new Clase();
+        cl.setNombre("POO");
+        cl.setCatedratico("Maestro x");
+        cl.setCodigo("cl-01");
+        cl.setPrecio(2300);
+        
+        Clase cl2 = new Clase();
+        cl2.setNombre("Biologia");
+        cl2.setCatedratico("Maestro xy");
+        cl2.setCodigo("cl-02");
+        cl2.setPrecio(2550);
+        ArrayList<Evento> evs = new ArrayList<Evento>();
+        evs.add(cl);
+        evs.add(cl2);
+        p2.setEvs(evs);
+
+        periodos.add(p2);
+
+        
+        c.setPeriodos(periodos);
+        c.SaveFile();
+        
+        Carrera d = new Carrera();
+        d.SetFromFile("Ing Financiera");
+        System.out.println(d.CalcularCosto());
     }
     
 }
