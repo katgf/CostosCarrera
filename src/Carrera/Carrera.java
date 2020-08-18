53aec6d98c1ad1391da1716e51747c8ab03b9ae8
katgf <Admin@DESKTOP-4C6I8TG>
2020-08-18 03:51:52 -0600

    agregada clase Carrera

---
+++ src/Carrera/Carrera.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Carrera;
+
+import archivos.archivos;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Admin
+ */
+public class Carrera {
+    private ArrayList<Periodo> Periodos;
+    private String Nombre;
+    private int añoInicio;
+
+    public Carrera() {
+    }
+
+    public ArrayList<Periodo> getPeriodos() {
+        return Periodos;
+    }
+
+    public void setPeriodos(ArrayList<Periodo> Periodos) {
+        this.Periodos = Periodos;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String Nombre) {
+        this.Nombre = Nombre;
+    }
+
+    public int getAñoInicio() {
+        return añoInicio;
+    }
+
+    public void setAñoInicio(int añoInicio) {
+        this.añoInicio = añoInicio;
+    }
+    
+    
+    public double CalcularCosto(){
+              double val=0;
+              for (Periodo Periodo : Periodos) 
+                  val +=Periodo.CalcularPeriodo();
+
+              return val;
+    }
+    
+    public void SaveFile(){
+            
+           archivos.writeFileTruncate(getNombre(), getNombre()+"-"+getAñoInicio()+"\n");
+           for (Periodo Periodo : Periodos) {
+            archivos.writeFileAppend(getNombre(), Periodo.NombrePeriodo()+";\n");
+            Periodo.SaveFile();
+        }
+
+    }
+    
+    public void SetFromFile(String Carrera){
+        ArrayList<String> lineas = archivos.ReadFile(Carrera);
+        ArrayList<Periodo> LoadPeriod=new ArrayList<Periodo>();
+       String infoGeneral[] = lineas.get(0).split("-");
+        setNombre(infoGeneral[0]);
+        setAñoInicio(Integer.parseInt(infoGeneral[1].replace(";","")));
+        lineas.remove(0);
+        for (String linea : lineas) {
+            Periodo thisp = new Periodo();
+            int A= Integer.parseInt(linea.split("-")[0]);
+            int P=Integer.parseInt(linea.split("-")[1].replace(";",""));
+            thisp.setAño(A);
+            thisp.setPeriodo(P);
+            thisp.SetFromFile(thisp.NombrePeriodo());
+            LoadPeriod.add(thisp);
+        }
+        this.setPeriodos(LoadPeriod);
+        
+    }
+    
+    
+ 
+}
